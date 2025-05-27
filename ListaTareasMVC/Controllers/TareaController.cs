304445bf8d271e9794c5b5285bc60b46df40bf0d
soyandreagonzalez <andreacgonzalezl9@gmail.com>
2025-05-26 20:05:14 -0500

    Agrego proyecto con .gitignore para ignorar .vs

---
+++ ListaTareasMVC/Controllers/TareaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ListaTareasMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListaTareasMVC.Controllers
+{
+    public class TareasController : Controller
+    {
+        private static List<Tarea> tareas = new();
+
+        public IActionResult Index()
+        {
+            return View(tareas);
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(string descripcion)
+        {
+            if (!string.IsNullOrEmpty(descripcion))
+            {
+                tareas.Add(new Tarea { Id = tareas.Count + 1, Descripcion = descripcion, Completada = false });
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            var tarea = tareas.FirstOrDefault(t => t.Id == id);
+            if (tarea != null)
+                tareas.Remove(tarea);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Completar(int id)
+        {
+            var tarea = tareas.FirstOrDefault(t => t.Id == id);
+            if (tarea != null)
+                tarea.Completada = !tarea.Completada;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ListaTareasMVC/Models/Tarea.cs
@@ -0,0 +1,9 @@
+﻿namespace ListaTareasMVC.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public bool Completada { get; set; }
+    }
+}
